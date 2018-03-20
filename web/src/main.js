94d7ce5df3c84c9e4a2a347116f8bfd8c0e4f9c3
walkerdepaula <walkerdepaula.work@gmail.com>
2018-03-20 20:56:50 -0300

    configurando /api para fazer os requests da aplicacao

--- web/src/main.js
+++ web/src/main.js
@@ -14,10 +14,16 @@ import store from './store'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
+const BASE_URL = process.env.BASE_URL
+
 import VueResource from 'vue-resource'
 
 Vue.use(VueMaterial);
 Vue.use(VueResource);
+
+Vue.http.options.crossOrigin = true
+Vue.http.options.xhr = {withCredentials: true}
+Vue.http.options.root = BASE_URL + 'api/'
 
 Vue.config.productionTip = false
 
